38f45022ccd3b21475c98f8c5d946249b5614cbb
Akira Hayatake <hayatake@waku-2.com>
2016-05-10 22:52:40 +0900

    テストコードの作成とバグ修正

--- am_modules/am-lunch-test/test/web/test.js
+++ am_modules/am-lunch-test/test/web/test.js
@@ -1,28 +1,40 @@
-var $ = (selector) => document.querySelectorAll(selector)
-
 Test.prototype.start({
   css_panic: () => {
     console.log("wonderful css panic!")
     console.info("finished");
   },
-  チートスコア: (score) => {
+  チートプレイ: (score) => {
     console.log("cheat play after 2000ms");
     var allClick = () => {
-      $enemy = $(".enemys")
-      for (var i = 0; i < $enemy.length; i++) {
-        $enemy[i].click()
+      $enemys = document.querySelectorAll(".enemys")
+      for (var i = 0; i < $enemys.length; i++) {
+        $enemys[i].click()
       }
       setTimeout(finalCheck, 2000)
     }
     var finalCheck = () => {
-      $scores = $(".score[name]")
+      $scores = document.querySelectorAll(".score[name]")
       console.assert($scores.length === score-0.0, `実際のスコアは${$scores.length}、判定のスコアは${score}`)
       console.info("finished")
     }
     setTimeout(allClick, 2000)
   },
-  真面目プレイ: () => {
-    $enemy = $(".enemys")
-    console.info("finished");
+  適切な位置に来たワニを叩く: ([num, timeoutMsec]) => {
+    num = num-0.0
+    timeoutMsec = timeoutMsec-0.0
+    var $enemys = document.querySelectorAll(".enemys")
+    var timeout = () => console.assert(!num, `叩いたワニ数は${num}`)
+    var test = () => {
+      if (!num) return console.info("finished")
+      var waniLocX = ""
+      for (var i = 0; i < $enemys.length; i++) {
+        var $enemy = $enemys[i];
+        if ($enemy.checked) continue
+        var protrudeFlg = Math.floor($enemy.getBoundingClientRect().bottom) > 330
+        if (protrudeFlg) return $enemy.click(--num)
+      }
+    }
+    setInterval(test, 50)
+    setTimeout(timeout, timeoutMsec)
   }
 })
